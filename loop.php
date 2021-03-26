c827b5b0481c164310c8f2ef3ad4f54c92947ee2
LiAnn2000 <s19t15911@stu.edu.tw>
2021-03-26 16:44:28 +0800

    0326

--- loop.php
+++ loop.php
@@ -1,13 +1,13 @@
 <?php
 
-$sum=0;
-$i=0;
-
-while(true){
-    $i++;
-    $sum += $i;
-    if ($sum >= 100) break;
-}
-printf("次数:%d",$i);
-printf("统计:%d",$sum);
+while (true){
+    echo "這是第一層迴圈！
+";
+    while (true){
+      echo "這是第二層迴圈！
+";
+      break 2;
+    }
+  }
+  echo "己跳出二層迴圈！";
 ?>
